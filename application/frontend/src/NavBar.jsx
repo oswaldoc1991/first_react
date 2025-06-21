a8498aae44ebd369ab943216e71183b1ad0a65a5
oswaldoc1991 <ocabrera@sfsu.edu>
2025-06-21 08:30:27 -0700

    going to work on laptop until i get ssd on pc

--- application/frontend/src/NavBar.jsx
+++ application/frontend/src/NavBar.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function NavBar() {
+    const [isOpen, setIsOpen] = useState(false)
     return (
     <nav className="bg-blue-600 text-white p-4 shadow -md">
         <div className="max-w-7xl mx-auto flex justify-between items-center">   
             <Link to="/mytasks" className="text-xl font-bold hover:text-gray-300">
                 MyTasks
             </Link>
-             <div className="space-x-4">
-                <Link to="/about" className="hover:text-gray-300">About | </Link>
-                <Link to="/login" className="hover:text-gray-300">Login</Link>
+
+            <button className="sm:hidden text-white focus:outline-non" onClick={() => setIsOpen(!isOpen)}>
+              ☰  
+            </button>
+
+
+             <div className={`sm:flex gap-4 ${isOpen ? "block m" : "hidden sm:block"}`}>
+                <Link to="/about" className="hover:text-gray-200 block sm:inline">
+                    About
+                </Link>
+                <Link to="/login" className="hover:text-gray-200 block sm:inline">
+                    Login
+                </Link>
+                <Link to="/get-started" className="hover:text-gray-200 block sm:inline">
+                    Get Started
+                </Link>
              </div>
         </div>
      </nav>
